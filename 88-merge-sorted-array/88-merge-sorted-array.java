8bfa2272f0d8cf329c15e2d5d8a848a768ad683c
Prachit Raorane <94752400+Prachit-Raorane@users.noreply.github.com>
2022-02-07 01:44:19 +0530

    Time: 0 ms (100.00%), Space: 43.1 MB (5.17%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -4,18 +4,17 @@ class Solution {
         public void merge(int[] nums1, int m, int[] nums2, int n) {
      
         
-        Queue<Integer> queue = new PriorityQueue<>();
-        
-         queue.
-                        addAll(Arrays.stream(nums1).limit(m).boxed().collect(Collectors.toList()));
-        queue.addAll(Arrays.stream(nums2).boxed().collect(Collectors.toList()));
-        
-        int i=0;
-        while(!queue.isEmpty()){
-            nums1[i] = queue.poll();
-            i++;
-        }
-        
+       int i = m + n -1;
+       int p1 = m - 1;
+       int p2 = n - 1;
+            
+       while(p2>=0){
+           if(p1>=0 && nums1[p1] > nums2[p2]){
+               nums1[i--] = nums1[p1--];
+           }else{
+               nums1[i--] = nums2[p2--];
+           }
+       }   
     }
     
 }
