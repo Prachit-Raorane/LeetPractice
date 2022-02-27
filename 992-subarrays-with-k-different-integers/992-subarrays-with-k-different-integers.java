cd2228f68518dc438f7f28fbe9011a487fee5e92
Prachit Raorane <94752400+Prachit-Raorane@users.noreply.github.com>
2022-02-27 18:59:04 +0530

    Time: 90 ms (39.54%), Space: 75.1 MB (34.69%) - LeetHub

---
+++ 992-subarrays-with-k-different-integers/992-subarrays-with-k-different-integers.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int subarraysWithKDistinct(int[] nums, int k) {
+        
+       return atMostKDistinct(nums,k) - atMostKDistinct(nums,k-1);
+    }
+    
+    
+    private int atMostKDistinct(int [] nums,int k){
+        int left =0;
+        
+        Map<Integer,Integer> freq = new HashMap<>();
+        int count =0;
+        for (int i=0;i < nums.length;i++){
+            
+            freq.put(nums[i],freq.getOrDefault(nums[i],0)+1);
+            
+            while(freq.size()>k){
+                freq.put(nums[left],freq.get(nums[left]) - 1);
+                if(freq.get(nums[left]) == 0){
+                    freq.remove(nums[left]);
+                }
+                left ++;
+            }
+            
+            count += (i-left+1);
+            
+        }
+        
+        return count;
+    }
+}
